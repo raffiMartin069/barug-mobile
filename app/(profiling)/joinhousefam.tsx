83aedb28f5122c888cf7ea3d95d772539e7c596d
hnnh-ae <hannahmae.dian@ctu.edu.ph>
2025-08-01 09:10:03 +0800

    join

--- app/(profiling)/joinhousefam.tsx
+++ app/(profiling)/joinhousefam.tsx
@@ -1,21 +1,38 @@
+import Spacer from '@/components/Spacer'
+import ThemedAppBar from '@/components/ThemedAppBar'
 import ThemedButton from '@/components/ThemedButton'
 import ThemedKeyboardAwareScrollView from '@/components/ThemedKeyboardAwareScrollView'
 import ThemedText from '@/components/ThemedText'
+import ThemedTextInput from '@/components/ThemedTextInput'
 import ThemedView from '@/components/ThemedView'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 const JoinHousehold = () => {
+  const [hhnum, setHhnum] = useState()
+
   return (
     <ThemedView safe={true}>
+      <ThemedAppBar
+        title='Join a Household'
+      />
       <ThemedKeyboardAwareScrollView>
         <View>
-          <ThemedText style={styles.text} title={true}>Join A Household</ThemedText>
+          <ThemedTextInput
+            placeholder='Household Number'
+            value={hhnum}
+            onChangeText={setHhnum}
+          />
+
+          <Spacer height={10}/>
+
+          <ThemedTextInput
+            placeholder='Family Number'
+            value={hhnum}
+            onChangeText={setHhnum}
+          />
         </View>
         <View>
-          <ThemedButton submit={false}>
-            <ThemedText non_btn={true}>Skip</ThemedText>
-          </ThemedButton>
           <ThemedButton>
             <ThemedText btn={true}>Continue</ThemedText>
           </ThemedButton>
