1ab41a46355f02506c2a826bdd1acd752e7fb556
zus6wyq9v <zus6wyq9v@mozmail.com>
2025-11-14 02:36:50 +0000

    chore: enhance UI and added edits and copies to allow users interact further with their sent queries

--- components/temp/bot/composer.tsx
+++ components/temp/bot/composer.tsx
@@ -1,6 +1,6 @@
 import { CHATBOT_COLORS, chatStyles } from "@/constants/temp/bot/chatbot";
 import { Ionicons } from "@expo/vector-icons";
-import { useColorScheme, View, TextInput, Pressable } from "react-native";
+import { Pressable, TextInput, useColorScheme, View } from "react-native";
 
 export const Composer: React.FC<{
     value: string;
